1af2b335d651d4084e052af44d8842e6011abebe
lktgt15 <64021540+lktgt15@users.noreply.github.com>
2020-08-20 10:56:36 +0900

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -18,16 +18,16 @@ public:
 		tmpv.resize(arr.size());
 	}
 
-	void merge(T l, T r, vector<T> &arr){
+	void merge(int l, int r, vector<T> &arr){
 		if(l >= r) return;
-		T mid = (l+r)/2;
+		int mid = (l+r)/2;
 		merge(l,mid,arr);
 		merge(mid+1,r,arr);
 		mergesort(l,r,arr);
 	}
 
-	void mergesort(T L, T R, vector<T> &arr){
-		T l,r,idx,mid;
+	void mergesort(int L, int R, vector<T> &arr){
+		int l,r,idx,mid;
 		l = idx = L;
 		mid = (L+R)/2;
 		r = mid+1;
@@ -40,10 +40,18 @@ public:
 		while(l <= mid) tmpv[idx++] = arr[l++];
 		while(r <= R) tmpv[idx++] = arr[r++];
 
-		for(T i=L;i<=R;i++) arr[i] = tmpv[i];
+		for(int i=L;i<=R;i++) arr[i] = tmpv[i];
 	}
 
 	void tmpToArrCopy(vector<T> &arr){
 		for(int i=0;i<arr.size();i++) arr[i] = tmpv[i];
 	}
 };
+
+int main(){
+	vector<int> arr = {9,6,7,5,3,2,8,4,1,0};
+	MergeSort<int> mst;
+	mst.sizing(arr);
+	mst.merge(0,arr.size()-1,arr);
+	for(int &n : arr) printf("%d ",n);
+}
