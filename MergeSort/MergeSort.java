b96a9c0117100b8a5fe221e6fd80afbb213b497c
lktgt15 <64021540+lktgt15@users.noreply.github.com>
2021-02-08 23:07:41 +0900

    Create MergeSort.java

---
+++ MergeSort/MergeSort.java
@@ -0,0 +1,36 @@
+package MergeSort;
+
+public class MergeSort {
+	private int[] tmparr;
+	
+	public void init(int length) {
+		if(tmparr == null || tmparr.length < length) tmparr = new int[length];
+	}
+	
+	public void sort(int[] arr,int l,int r) {
+		init(arr.length);
+		split(arr,l,r);
+	}
+	
+	public void split(int[] arr,int l,int r) {
+		if(l+1 >= r) return;
+		int mid = (l+r)/2;
+		
+		split(arr, l, mid);
+		split(arr, mid, r);
+		mergeSort(arr, l, r);
+	}
+	
+	public void mergeSort(int[] arr,int l,int r) {
+		int tmpl = l, mid = (l+r)/2, tmpr = (l+r)/2;
+		int idx = l;
+		while(tmpl < mid && tmpr < r) {
+			if(arr[tmpl] < arr[tmpr]) tmparr[idx++] = arr[tmpl++];
+			else tmparr[idx++] = arr[tmpr++];
+		}
+		while(tmpl < mid) tmparr[idx++] = arr[tmpl++];
+		while(tmpr < r) tmparr[idx++] = arr[tmpr++];
+		
+		for(int i=l;i<r;i++) arr[i] = tmparr[i];
+	}
+}
